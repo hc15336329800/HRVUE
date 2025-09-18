5e43f841cb2a002a21111e2c6cd133a895b83181
zgr <462909138@qq.com>
2025-09-18 10:16:19 +0800

    chore: cleanup tracked cache/build dirs

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import App from './App.vue';
 import router from './router/index.js';
 
 const app = createApp(App);
 
 app.use(router);
-
-if (window.ElementPlus) {
-  app.use(window.ElementPlus);
-}
+app.use(ElementPlus);
 
 app.mount('#app');
